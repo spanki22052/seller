7ddbbe2b9128e87cceda09052ee6ff658bc751ba
spanki22052 <useful22052@gmail.com>
2026-01-01 17:13:27 +0300

    another fix

--- client/src/app/product/page.tsx
+++ client/src/app/product/page.tsx
@@ -1,6 +1,28 @@
+import { Suspense } from "react";
 import { ProductPage } from "@/pages/ProductPage";
 
+function ProductPageFallback() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#050505",
+        color: "#ededed",
+      }}
+    >
+      <div>Loading...</div>
+    </div>
+  );
+}
+
 export default function Product() {
-  return <ProductPage />;
+  return (
+    <Suspense fallback={<ProductPageFallback />}>
+      <ProductPage />
+    </Suspense>
+  );
 }
 
